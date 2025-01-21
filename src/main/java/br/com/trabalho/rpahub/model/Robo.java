440f35eca8f62e17a7fdc11cecc7a32e9b5909b8
Davi Trajano <90059259+davi-tr@users.noreply.github.com>
2025-01-21 19:11:49 -0300

    iniciacilização das demais classes

---
+++ src/main/java/br/com/trabalho/rpahub/model/Robo.java
@@ -0,0 +1,4 @@
+package br.com.trabalho.rpahub.model;
+
+public class Robo {
+}
---
+++ src/main/java/br/com/trabalho/rpahub/model/VM.java
@@ -0,0 +1,11 @@
+package br.com.trabalho.rpahub.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VM {
+}
